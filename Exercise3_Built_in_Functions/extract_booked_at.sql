42f1e3a1c59ea1b69fa3571c59b14a99baaf80c7
AngEvd <164805178+AngEvd@users.noreply.github.com>
2025-01-12 19:45:06 +0200

    Update extract_booked_at.sql

--- Exercise3_Built_in_Functions/extract_booked_at.sql
+++ Exercise3_Built_in_Functions/extract_booked_at.sql
@@ -1,8 +1,8 @@
 SELECT
-YEAR(booked_at) AS "YEAR",
-MONTH(booked_at) AS "MONTH",
-DAY(booked_at) AS "DAY",
-HOUR(booked_at) AS "HOUR",
-MINUTE(booked_at) AS "MINUTE",
-SECOND(booked_at) AS "SECOND"
-FROM booked_at;
+    EXTRACT(YEAR FROM booked_at) AS "YEAR",
+    EXTRACT(MONTH FROM booked_at) AS "MONTH",
+    EXTRACT(DAY FROM booked_at) AS "DAY",
+    EXTRACT(HOUR FROM booked_at AT TIME ZONE 'UTC') AS "HOUR",
+    EXTRACT(MINUTE FROM booked_at) AS "MINUTE",
+    CEILING(EXTRACT(SECOND FROM booked_at)) AS "SECOND"
+FROM bookings;
