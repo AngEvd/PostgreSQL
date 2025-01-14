9bf753ef30f87de42f64e123a78744f4056ac18f
AngEvd <164805178+AngEvd@users.noreply.github.com>
2025-01-14 20:21:01 +0200

    Add files via upload

---
+++ Lab5_Table_relations/mountains_and_peaks.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mountains (
+		id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+		name VARCHAR(50)
+);
+
+CREATE TABLE peaks (
+		id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+		name VARCHAR(50),
+		mountain_id INT,
+		CONSTRAINT fk_peaks_mountains
+		FOREIGN KEY (mountain_id)
+			REFERENCES mountains (id)
+);
+
+
